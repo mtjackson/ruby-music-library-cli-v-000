c1c5ab2ca5c371ace8bd276e623f699c79cf1d8b
Mary Jackson <jackson.marytherese@gmail.com>
2018-06-07 23:42:34 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -99,16 +99,17 @@ class MusicLibraryController
   end
 
   def play_song
+    i = 1
+    song_list = []
+
+    self.new_music_importer.each do |song|
+      song_list << song
+    end
     puts self.list_songs
     puts "Which song number would you like to play?"
     input = gets.strip
 
-  #  i = 1
-  #  song_list = []
 
-  #  self.new_music_importer.each do |song|
-  #    song_list << song
-  #  end
 
   #  if input <= self.song_list.length
   #    puts "Playing #{input}"
